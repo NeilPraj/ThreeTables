25693c8ddfe7f91680e1b74df6f6a9fef0dd6fab
NeilPraj <neilpraj@gmail.com>
2022-10-23 14:03:38 -0500

    Added ExceptionHandling and delete

--- src/main/java/com/example/threetables/controllers/ItemController.java
+++ src/main/java/com/example/threetables/controllers/ItemController.java
@@ -1,6 +1,8 @@
 package com.example.threetables.controllers;
 
 import com.example.threetables.entities.Item;
+import com.example.threetables.entities.Owner;
+import com.example.threetables.exceptions.ResourceNotFoundException;
 import com.example.threetables.repos.ItemRepository;
 import com.example.threetables.repos.OwnerRepository;
 import com.example.threetables.repos.TypeRepository;
@@ -9,7 +11,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class ItemController {
@@ -31,8 +35,32 @@ public class ItemController {
 
     @GetMapping("/items/name")
     public ResponseEntity<List<Item>> findItemByName(@RequestParam String name){
+
         return new ResponseEntity<List<Item>>(itemRepo.findByName(name), HttpStatus.OK);
     }
+
+    /*
+    @DeleteMapping("/items/deleteitem")
+    public ResponseEntity<List<Item>> deleteItemByName(@RequestParam String name){
+        List<Item> items= itemRepo.findByName(name);
+        return new ResponseEntity<List<Item>>(itemRepo.delete((Item) items), HttpStatus.OK);
+    }
+
+     */
+
+    @DeleteMapping("/items/{id}")
+    public Map<String, Boolean> deleteEmployee(@PathVariable(value = "id") Long employeeId)
+            throws ResourceNotFoundException {
+        Item item = itemRepo.findById(employeeId)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee not found for this id :: " + employeeId));
+
+        itemRepo.delete(item);
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("deleted", Boolean.TRUE);
+        return response;
+    }
+
+
 
 
 
--- src/main/java/com/example/threetables/repos/OwnerRepository.java
+++ src/main/java/com/example/threetables/repos/OwnerRepository.java
@@ -1,7 +1,12 @@
 package com.example.threetables.repos;
 
+import com.example.threetables.entities.Item;
 import com.example.threetables.entities.Owner;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface OwnerRepository extends JpaRepository<Owner, Long> {
+
+
 }
